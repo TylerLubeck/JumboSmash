2f8f45c59d4a23ef907f9b1cfc99f4348f7339e9
Tyler Lubeck <tyler@tylerlubeck.com>
2015-05-05 11:37:00 -0400

    headshot upload path

--- smashers/models.py
+++ smashers/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+import os
 
+
+def file_path(instance, filename):
+    return os.path.join('headshots', instance.user.username, filename)
 
 
 class UserProfile(models.Model):
@@ -18,18 +22,12 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, null=True, blank=True)
 
-    headshot = ProcessedImageField(upload_to='headshots',
+    headshot = ProcessedImageField(upload_to=file_path,
                                    processors=[ResizeToFill(300, 300)],
                                    null=True,
                                    blank=True)
 
-    # headshot = models.ImageField(upload_to='headshots',
-    #                              null=True,
-    #                              blank=True)
-
     has_headshot = models.BooleanField(default=False)
-
-
 
     people_i_like = models.ManyToManyField('self',
                                            symmetrical=False,
@@ -41,10 +39,10 @@ class UserProfile(models.Model):
                                                 related_name='people_dont_like_me',
                                                 blank=True)
 
-
     def is_registered(self):
-        return self.user != None
+        return self.user is not None
     is_registered.short_description = 'Registered'
     is_registered.boolean = True
+
     def __str__(self):
         return self.name
