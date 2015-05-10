36c40562a01f265796b54bec94afafc05672ab35
Tyler Lubeck <tyler@tylerlubeck.com>
2015-05-10 16:52:42 -0400

    added print statements so I can see if it works

--- smashers/management/commands/notify_matches.py
+++ smashers/management/commands/notify_matches.py
@@ -3,6 +3,7 @@ from smashers.models import UserProfile
 from django.core.mail import send_mail
 from django.template.loader import get_template
 from django.template import Context
+import smptlib
 
 class Command(BaseCommand):
     help = 'Loads the seniors from the json returned by the scraper'
@@ -29,7 +30,12 @@ class Command(BaseCommand):
         plaintext = get_template('matches_found.txt')
         text_content = plaintext.render(d)
 
-        send_mail(title,
-                  text_content,
-                  'jumbosmashers2@gmail.com',
-                  [user_profile.email])
+        print 'Emailing {}'.format(user_profile.name)
+        try:
+            send_mail(title,
+                    text_content,
+                    'jumbosmashers2@gmail.com',
+                    [user_profile.email],
+                    fail_silently=False)
+        except smptlib.SMTPException:
+            print 'Failed to email {}'.format(user_profile.name)
