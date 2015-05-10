362f5672d3cc79cecb772a15e116215d35582c5e
Tyler Lubeck <tyler@tylerlubeck.com>
2015-05-10 16:17:21 -0400

    secondary email address

--- JumboSmash/settings.py
+++ JumboSmash/settings.py
@@ -149,7 +149,7 @@ if DEBUG:
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jumbosmashers@gmail.com'
+EMAIL_HOST_USER = 'jumbosmashers2@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('JUMBOSMASHERS_PW', None)
 if EMAIL_HOST_PASSWORD is None and not DEBUG:
     raise Exception('No email password found!')
